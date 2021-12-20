53d3206aee651a9a75777d6c575252a24d81cbb2
Bilgehan Gecici <bilgehangecici@gmail.com>
2021-12-21 01:28:50 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/windowapp/MainActivity.java
+++ app/src/main/java/com/example/windowapp/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.windowapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,16 +17,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         VideoView videoview = (VideoView) findViewById(R.id.videoView);
         Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.background_video);
         videoview.setVideoURI(uri);
         videoview.start();
+        Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firebase(view);
+            }
+        });
         videoview.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mp) {
                 mp.setLooping(true);
             }
         });
+    }
+
+    public void firebase(View view) {
+        Intent firebaseTest = new Intent(this, FirebaseActivity.class);
+        startActivity(firebaseTest);
+
     }
 }
