f1b5a6726496be4948940a3076c1d5e9be7ca972
mehmetaliyuksel <mehmetaliyuksel5@gmail.com>
2021-12-21 01:16:24 +0300

    Upgrade and Update gradle, Implement ExoPlayer

--- app/src/main/java/com/example/windowapp/VideoPlayer.java
+++ app/src/main/java/com/example/windowapp/VideoPlayer.java
@@ -1,28 +1,35 @@
 package com.example.windowapp;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.MediaController;
 import android.widget.VideoView;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.ui.PlayerView;
 
-import java.io.IOException;
 
-public class VideoPlayer extends AppCompatActivity implements MediaPlayer.OnCompletionListener {
+public class VideoPlayer extends AppCompatActivity implements MediaPlayer.OnCompletionListener, Player.Listener {
     //private VideoView videoView;
+    ExoPlayer player;
+    PlayerView playerView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_player);
 
-        playRandomVideo();
+        //playRandomVideo();
+
+        player = new ExoPlayer.Builder(this).build();
+        playerView = new PlayerView(this);
+        playerView = (PlayerView) findViewById(R.id.videoView2);
+        playRandomEP();
 
     }
 
@@ -31,7 +38,6 @@ public class VideoPlayer extends AppCompatActivity implements MediaPlayer.OnComp
         MediaController mediaController = new MediaController(this);
         mediaController.setAnchorView(videoView);
         mediaController.setMediaPlayer(videoView);
-        //mediaController. media controller buttons will be set or hid 
         setVideoFromURL(videoView);
         videoView.setMediaController(mediaController);
         videoView.setOnCompletionListener(this);
@@ -39,21 +45,40 @@ public class VideoPlayer extends AppCompatActivity implements MediaPlayer.OnComp
 
     }
 
+    public void playRandomEP() {
+        playerView.setPlayer(player);
+        String uriPath = "https://firebasestorage.googleapis.com/v0/b/windowapp-335316.appspot.com/o/2519717394.mp4?alt=media";
+        MediaItem mediaItem = MediaItem.fromUri(uriPath);
+        player.setMediaItem(mediaItem);
+        player.addListener(this);
+        player.prepare();
+        player.setPlayWhenReady(true);
+    }
+
     public void setVideoFromURL(VideoView videoView) {
-        //String uriPath = "https://www.youtube.com/watch?v=dE1P4zDhhqw";
         //String uriPath = "https://media.geeksforgeeks.org/wp-content/uploads/20201217192146/Screenrecorder-2020-12-17-19-17-36-828.mp4?_=1";
         String uriPath = "https://firebasestorage.googleapis.com/v0/b/windowapp-335316.appspot.com/o/2519717394.mp4?alt=media";
         Uri uri = Uri.parse(uriPath);
-        //mp.setDataSource(getApplicationContext(), uri);
-        //videoView.setVideoPath("https://www.youtube.com/watch?v=dE1P4zDhhqw");
         videoView.setVideoURI(uri);
-
     }
 
     @Override
     public void onCompletion(MediaPlayer mediaPlayer) {
         Intent intentMain = new Intent(this, MainActivity.class);
         startActivity(intentMain);
-
     }
+
+    private void returnToMainActivity() {
+        Intent intentMain = new Intent(this, MainActivity.class);
+        startActivity(intentMain);
+    }
+
+    @Override
+    public void onIsPlayingChanged(boolean isPlaying) {
+        if (!isPlaying && player.getPlaybackState() == Player.STATE_ENDED){
+            player.release();
+            returnToMainActivity();
+        }
+    }
+
 }
